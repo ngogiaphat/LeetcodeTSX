622bc813356099a79c1102582c9adbad29a20c8d
ngogiaphat <ngogiaphat0802@gmail.com>
2023-04-15 17:01:58 +0700

    5h01PM 15/04/2023

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,12 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        
+      },
     },
   },
-  plugins: [],
+  plugins: [
+
+  ],
 }
