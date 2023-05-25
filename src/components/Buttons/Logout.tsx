04adc9c0b9cf93825c8c1d62e0e05acef55e3310
ngogiaphat <ngogiaphat0802@gmail.com>
2023-05-26 01:09:00 +0700

    add the package

--- src/components/Buttons/Logout.tsx
+++ src/components/Buttons/Logout.tsx
@@ -3,14 +3,14 @@ import React from "react";
 import { useSignOut } from "react-firebase-hooks/auth";
 import { FiLogOut } from "react-icons/fi";
 const Logout: React.FC = () => {
-    const [signOut, loading, error] = useSignOut(auth);
-    const handleLogout = () => {
-        signOut();
-    };
-    return(
-        <button className = 'bg-dark-fill-3 py-1.5 px-3 cursor-pointer rounded text-brand-orange' onClick = { handleLogout }>
+	const [signOut, loading, error] = useSignOut(auth);
+	const handleLogout = () => {
+		signOut();
+	};
+	return(
+		<button className = 'bg-dark-fill-3 py-1.5 px-3 cursor-pointer rounded text-brand-orange' onClick = {handleLogout}>
 			<FiLogOut />
-		</button>    
-    );
+		</button>
+	);
 };
 export default Logout;
